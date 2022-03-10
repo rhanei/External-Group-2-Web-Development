20fa1147acbb6cc275eb98210547c28ec072a361
hellokjy11 <72700837+hellokjy11@users.noreply.github.com>
2022-03-10 20:32:35 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,104 @@
+import os #디렉토리 절대 경로
+from flask import Flask
+from flask import render_template #template폴더 안에 파일을 쓰겠다
+from flask import request #회원정보를 제출할 때 쓰는 request, post요청 처리
+from flask import redirect #리다이렉트
+#from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models import User
+from flask import session #세션
+from flask_wtf import FlaskForm 
+from flask_wtf.csrf import CSRFProtect #csrf
+from forms import RegisterForm, LoginForm
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    userid = session.get('userid',None)
+    return render_template('main.html', userid=userid)
+    
+
+@app.route('/register', methods=['GET', 'POST']) #GET(정보보기), POST(정보수정) 메서드 허용
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit(): #유효성 검사. 내용 채우지 않은 항목이 있는지까지 체크
+        usertable = User(form.data.get('userid'))
+        #usertable.userid = form.data.get('axieid')
+        #usertable.email = form.data.get('email')
+        #usertable.password = form.data.get('password')
+
+        db.session.add(usertable) #DB저장
+        db.session.commit() #변동사항 반영
+        print("Registered successfully")
+
+        return redirect('/')
+    return render_template('register.html', form=form) #form이 어떤 form인지 명시한다
+
+@app.route('/login', methods=['GET','POST'])  
+def login():
+    form = LoginForm(request.form) #로그인폼
+    #userid = request.form['userid']
+    
+    if form.validate_on_submit(): #유효성 검사
+        print('{}가 로그인 했습니다'.format(form.data.get('userid')))
+        session['userid']=form.data.get('userid') #form에서 가져온 userid를 세션에 저장
+        return redirect('/') #성공하면 main.html로
+    return render_template('login.html', form=form)
+ 
+
+
+    #if form.validate_on_submit(): #유효성 검사
+    #userid = request.form.get('userid', False)
+    #return 'Axie ID {}, welcome!'.format(form.data.get(userid)) and render_template('login.html', form = form)
+
+    #if form.validate_on_submit(): #유효성 검사
+        #print('{}가 로그인 했습니다'.format(form.data.get('userid')))
+        #session['userid']=form.data.get('userid') #form에서 가져온 userid를 세션에 저장
+        #return redirect('/') #성공하면 main.html로
+    
+    #return render_template('login.html', form=form)
+
+
+
+
+
+
+    #if request.method == 'POST' and form.validate_on_submit():
+    #        error = None
+    #        user = User.query.filter_by(username=form.username.data).first()
+    #       if not user:
+    #          error = "존재하지 않는 사용자입니다."
+
+       #     else: # error is None:
+       #         session.clear()
+       #         session['user_id'] = user.id
+       #         return render_template('main.index')
+       #     #flash(error)
+    #return render_template('login.html', form=form)
+
+
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('userid', None)
+    return redirect('/')
+
+if __name__ == "__main__":
+    #데이터베이스---------
+    basedir = os.path.abspath(os.path.dirname(__file__)) #현재 파일이 있는 디렉토리 절대 경로
+    dbfile = os.path.join(basedir, 'db.sqlite') #데이터베이스 파일을 만든다
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #추가 메모리를 사용하므로 꺼둔다
+    app.config['SECRET_KEY']='asdfasdfasdfqwerty' #해시값은 임의로 적음
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
+    db.init_app(app) #app설정값 초기화
+    db.app = app #Models.py에서 db를 가져와서 db.app에 app을 명시적으로 넣는다
+    db.create_all() #DB생성
+
+    app.run(host="127.0.0.1", port=5000, debug=True)
